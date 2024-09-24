c9335bf522ed77d15222a4ef0a5853cd08230572
Izaiyah Stokes <118138305+Zero-th@users.noreply.github.com>
2024-09-23 23:09:37 -0700

    [ updated examples\winXweb to use new macro ]

--- examples/winXweb/win_x_web.c
+++ examples/winXweb/win_x_web.c
@@ -6,12 +6,12 @@ bool main_loop(void* _) {
         if( event.type == SDL_QUIT ) { lotus_exit(); break; }
     }; lotus_render((lotus_draw_call){ .mode=DRAW_TRIANGLES });
     return 0;
-}; void web_loop() { main_loop(NULL); }
+}; lotus_web_wrapper(main_loop);
 
 int main(void) {
     lotus_init();
-    lotus_set_web_main(web_loop);
     lotus_set_win_main(main_loop);
+    lotus_set_web_main(main_loop_web);
     lotus_main();
     lotus_exit();
     return 0;
